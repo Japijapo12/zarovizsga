052fbc39debb69151e859767884a3e28521c0ba2
Edina <terjek.edina@gmail.com>
2021-03-16 20:35:58 +0100

    Kutyák

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/Kennel.java
@@ -0,0 +1,51 @@
+package hu.nive.ujratervezes.zarovizsga.dogtypes;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Kennel {
+
+    private Dog dog;
+
+    List<Dog> dogs = new ArrayList<>();
+
+    public void addDog(Dog dog) {
+        dogs.add(dog);
+    }
+
+    public void feedAll() {
+          for (Dog dog : dogs) {
+              dog.feed();
+          }
+    }
+
+    public Dog findByName(String name) {
+        for (Dog dog : dogs) {
+            if(dog.getName().equals(name)) {
+                return dog;
+            }
+        }
+        throw new IllegalStateException("Nincs ilyen nevű kutya: " + name);
+    }
+
+
+    public void playWith(String name, int hours) {
+        for (Dog dog : dogs) {
+            findByName(name).play(hours);
+        }
+    }
+
+
+    public List<String> getHappyDogNames(int minHappiness) {
+        List<String> result = new ArrayList<>();
+        for (Dog dog : dogs) {
+            if( dog.getHappiness() > minHappiness) {
+                result.add(dog.getName());
+            }
+        }
+        throw new IllegalStateException("Nincs ennél az értéknél " + minHappiness + " boldogabb kutya");
+    }
+
+}
+
+
