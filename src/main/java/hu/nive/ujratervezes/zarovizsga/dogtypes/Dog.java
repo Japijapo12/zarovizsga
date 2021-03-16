26ee69d921d7811cccffaa60859b1f9306ae510c
Edina <terjek.edina@gmail.com>
2021-03-16 20:35:37 +0100

    Kutyák

---
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/dogtypes/Dog.java
@@ -0,0 +1,33 @@
+package hu.nive.ujratervezes.zarovizsga.dogtypes;
+
+public class Dog {
+
+    private String name;
+    private int happiness = 0;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public void setHappiness(int happiness) {
+        this.happiness = happiness;
+    }
+
+    public void feed() {
+
+    }
+
+    public void play(int hours) {
+        this.happiness =  this.happiness * hours;
+
+    }
+
+}
